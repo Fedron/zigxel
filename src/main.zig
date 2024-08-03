11c22cd009c9242ceaf477c60fbd54c014ea7449
Nicolas Fedor <fedronn@gmail.com>
2024-08-03 14:31:24 +0100

    feat: draw a quad

--- src/main.zig
+++ src/main.zig
@@ -12,46 +12,25 @@ fn logGLFWError(error_code: glfw.ErrorCode, description: [:0]const u8) void {
 /// Procedure table that will hold loaded OpenGL functions.
 var gl_procs: gl.ProcTable = undefined;
 
-/// ```txt
-///               (5)
-///             ..'''..
-///         ..''       ''..
-/// (3) ..''_______________''.. (4)
-///     |\                   /|
-///     |  \               /  |
-///     |    \           /    |
-///     |     \         /     |
-/// (1) ''..    \     /    ..'' (2)
-///         ''..  \ /  ..''
-///             ''...''
-///               (0)
-/// ```
-const hexagon_mesh = struct {
+const Vertex = struct {
+    position: Position,
+    color: Color,
+
+    const Position = [3]f32;
+    const Color = [3]f32;
+};
+
+const quad_mesh = struct {
     // zig fmt: off
-    const vertices = [_]Vertex{
-        .{ .position = .{  0    , -1   }, .color = .{ 0, 1, 1 } },
-        .{ .position = .{ -0.866, -0.5 }, .color = .{ 0, 0, 1 } },
-        .{ .position = .{  0.866, -0.5 }, .color = .{ 0, 1, 0 } },
-        .{ .position = .{ -0.866,  0.5 }, .color = .{ 1, 0, 1 } },
-        .{ .position = .{  0.866,  0.5 }, .color = .{ 1, 1, 0 } },
-        .{ .position = .{  0    ,  1   }, .color = .{ 1, 0, 0 } },
+    const vertices = [4]Vertex{
+        .{ .position = .{ -0.5, 0.5, 0.0 }, .color = .{ 1.0, 0.0, 0.0 } },
+        .{ .position = .{ 0.5, 0.5, 0.0 }, .color = .{ 0.0, 1.0, 0.0 } },
+        .{ .position = .{ 0.5, -0.5, 0.0 }, .color = .{ 0.0, 0.0, 1.0 } },
+        .{ .position = .{ -0.5, -0.5, 0.0 }, .color = .{ 1.0, 0.0, 1.0 } },
     };
     // zig fmt: on
 
-    const indices = [_]u8{
-        0, 3, 1,
-        0, 4, 3,
-        0, 2, 4,
-        3, 4, 5,
-    };
-
-    const Vertex = extern struct {
-        position: Position,
-        color: Color,
-
-        const Position = [2]f32;
-        const Color = [3]f32;
-    };
+    const indices = [6]u8{ 0, 1, 3, 1, 2, 3 };
 };
 
 pub fn main() !void {
@@ -64,7 +43,7 @@ pub fn main() !void {
     defer glfw.terminate();
 
     // Create our window, specifying that we want to use OpenGL.
-    const window = glfw.Window.create(640, 480, "mach-glfw + OpenGL", null, null, .{
+    const window = glfw.Window.create(1280, 720, "zigxel", null, null, .{
         .context_version_major = gl.info.version_major,
         .context_version_minor = gl.info.version_minor,
         .opengl_profile = .opengl_core_profile,
@@ -95,42 +74,24 @@ pub fn main() !void {
     // The window and OpenGL are now both fully initialized.
 
     const vertex_shader_source: [:0]const u8 =
-        \\#version 410 core
-        \\
-        \\// Width/height of the framebuffer (= the window).
-        \\uniform vec2 u_FramebufferSize;
-        \\
-        \\// Angle of the object we're drawing, in radians.
-        \\uniform float u_Angle;
+        \\#version 460 core
         \\
         \\// Vertex position and color as defined in the mesh.
-        \\in vec4 a_Position;
-        \\in vec4 a_Color;
+        \\in vec3 a_Position;
+        \\in vec3 a_Color;
         \\
         \\// Color result that will be passed to the fragment shader.
         \\out vec4 v_Color;
         \\
         \\void main() {
-        \\    // Account for the window's aspect ratio. We want a 1:1 width/height ratio.
-        \\    float scaleX = min(u_FramebufferSize.y / u_FramebufferSize.x, 1);
-        \\    float scaleY = min(u_FramebufferSize.x / u_FramebufferSize.y, 1);
-        \\
-        \\    float s = sin(u_Angle);
-        \\    float c = cos(u_Angle);
-        \\
-        \\    gl_Position = vec4(
-        \\        (a_Position.x * c + a_Position.y * -s) * scaleX,
-        \\        (a_Position.x * s + a_Position.y * c) * scaleY,
-        \\        a_Position.zw
-        \\    ) * vec4(0.875, 0.875, 1, 1); // Shrink the object slightly to fit the window.
-        \\
+        \\    gl_Position = vec4(a_Position.xyz, 1.0);
         \\    // Pass the vertex's color to the fragment shader.
-        \\    v_Color = a_Color;
+        \\    v_Color = vec4(a_Color.xyz, 1.0);
         \\}
         \\
     ;
     const fragment_shader_source: [:0]const u8 =
-        \\#version 410 core
+        \\#version 460 core
         \\
         \\// Color input from the vertex shader.
         \\in vec4 v_Color;
@@ -202,9 +163,6 @@ pub fn main() !void {
     };
     defer gl.DeleteProgram(program);
 
-    const framebuffer_size_uniform = gl.GetUniformLocation(program, "u_FramebufferSize");
-    const angle_uniform = gl.GetUniformLocation(program, "u_Angle");
-
     // Vertex Array Object (VAO), remembers instructions for how vertex data is laid out in memory.
     // Using VAOs is strictly required in modern OpenGL.
     var vao: c_uint = undefined;
@@ -235,8 +193,8 @@ pub fn main() !void {
             // Upload vertex data to the VBO.
             gl.BufferData(
                 gl.ARRAY_BUFFER,
-                @sizeOf(@TypeOf(hexagon_mesh.vertices)),
-                &hexagon_mesh.vertices,
+                @sizeOf(@TypeOf(quad_mesh.vertices)),
+                &quad_mesh.vertices,
                 gl.STATIC_DRAW,
             );
 
@@ -245,11 +203,11 @@ pub fn main() !void {
             gl.EnableVertexAttribArray(position_attrib);
             gl.VertexAttribPointer(
                 position_attrib,
-                @typeInfo(hexagon_mesh.Vertex.Position).Array.len,
+                @typeInfo(Vertex.Position).Array.len,
                 gl.FLOAT,
                 gl.FALSE,
-                @sizeOf(hexagon_mesh.Vertex),
-                @offsetOf(hexagon_mesh.Vertex, "position"),
+                @sizeOf(Vertex),
+                @offsetOf(Vertex, "position"),
             );
 
             // Ditto for vertex colors.
@@ -257,11 +215,11 @@ pub fn main() !void {
             gl.EnableVertexAttribArray(color_attrib);
             gl.VertexAttribPointer(
                 color_attrib,
-                @typeInfo(hexagon_mesh.Vertex.Color).Array.len,
+                @typeInfo(Vertex.Color).Array.len,
                 gl.FLOAT,
                 gl.FALSE,
-                @sizeOf(hexagon_mesh.Vertex),
-                @offsetOf(hexagon_mesh.Vertex, "color"),
+                @sizeOf(Vertex),
+                @offsetOf(Vertex, "color"),
             );
         }
 
@@ -269,13 +227,11 @@ pub fn main() !void {
         gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
         gl.BufferData(
             gl.ELEMENT_ARRAY_BUFFER,
-            @sizeOf(@TypeOf(hexagon_mesh.indices)),
-            &hexagon_mesh.indices,
+            @sizeOf(@TypeOf(quad_mesh.indices)),
+            &quad_mesh.indices,
             gl.STATIC_DRAW,
         );
     }
-
-    var timer = try std.time.Timer.start();
 
     main_loop: while (true) {
         glfw.pollEvents();
@@ -285,26 +241,17 @@ pub fn main() !void {
 
         {
             // Clear the screen to white.
-            gl.ClearColor(1, 1, 1, 1);
+            gl.ClearColor(0.2, 0.2, 0.2, 1.0);
             gl.Clear(gl.COLOR_BUFFER_BIT);
 
             gl.UseProgram(program);
             defer gl.UseProgram(0);
 
-            // Make sure any changes to the window's size are reflected.
-            const framebuffer_size = window.getFramebufferSize();
-            gl.Viewport(0, 0, @intCast(framebuffer_size.width), @intCast(framebuffer_size.height));
-            gl.Uniform2f(framebuffer_size_uniform, @floatFromInt(framebuffer_size.width), @floatFromInt(framebuffer_size.height));
-
-            // Rotate the hexagon clockwise at a rate of one complete turn per minute.
-            const seconds = @as(f32, @floatFromInt(timer.read())) / std.time.ns_per_s;
-            gl.Uniform1f(angle_uniform, seconds / 60 * -std.math.tau);
-
             gl.BindVertexArray(vao);
             defer gl.BindVertexArray(0);
 
             // Draw the hexagon!
-            gl.DrawElements(gl.TRIANGLES, hexagon_mesh.indices.len, gl.UNSIGNED_BYTE, 0);
+            gl.DrawElements(gl.TRIANGLES, quad_mesh.indices.len, gl.UNSIGNED_BYTE, 0);
         }
 
         window.swapBuffers();
